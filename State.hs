60e64849f78611b9122abdf2483f3838926fadc0
Sean Seefried <sean.seefried@gmail.com>
2018-01-08 22:35:55 +1100

    Modify

--- State.hs
+++ State.hs
@@ -3,83 +3,106 @@ module State where
 import Prelude hiding (read)
 import Control.Applicative
 
-data S s a = S { runS :: s -> (a, s) } 
+data S s a = S { runS :: s -> (a, s) }
+-- runS :: S s a -> s -> (a, s)
 
 instance Monad (S s) where
-  -- return :: a -> S a
-  return a = S (\s -> (a,s))
-  -- (>>=) :: S a -> (a -> S b) -> S b 
+  -- return :: a -> S s a
+  return a = S $ \s -> (a, s)
+  -- (>>=) :: S s a -> (a -> S s b) -> S s b
   S f >>= k = 
-    -- f :: s -> (Int, s)
+    -- f :: s -> (a, s)
+    -- k :: a -> S s b
     S $ \s -> let (a, s') = f s 
-               in runS (k a) s'
-    -- k a           :: S b
-    -- runS (k a)    :: s -> (b,s)
-    -- runS (k a) s' :: (b, s)
+               in runS (k a) s' -- s -> (b, s)
+    -- k a            :: S s b
+    -- runS (k a)     :: s -> (b, s)
+    -- runS (k a) s'  :: (b, s)
 
 read :: S s s
-read = S (\s -> (s,s))
+read = S $ \s -> (s,s)
 
 write :: s -> S s ()
-write s' = S (\s -> ((), s'))
+write s = S $ \_ -> ((), s)
 
 foo :: S Int Int
-foo = do
-  x <- read
-  write (x+1)
-  x <- read
-  return x
+foo = do x <- read
+         write (x+1)
+         x <- read
+         return (x+3)
 
-test :: (Int, Int)
+test :: (Int, Int)         
 test = runS foo 0
 
 {-
 
-instance Monad (\Int -> (_,Int)) where
+instance Monad (\s -> (_, s)) where
   -- return :: a -> (s -> (a,s))
   return a = \s -> (a,s)
   -- (>>=) :: (s -> (a,s)) -> (a -> (s -> (b,s))) -> (s -> (b,s))
-  f >>= k = 
-    -- f :: s -> (Int, s)
-    \s -> let (a, s') = f s in k a s'
+  f >>= k = \s -> let (a, s') = f s in k a s'
 
 read s = (s,s)
-write s' = \s -> ((), s')
+write s = \_ -> ((), s)
 
-foo = read >>= (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return x)))
+foo = 
+  read >>= \x ->
+  write (x+1) >>= \_ ->
+  read >>= \x ->
+  return x
+
+-- equivalently
+foo = read >>= (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return (x+3)))
 
 test = foo 0
-     = (read >>= (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return x)))) 0
-     = (\s -> let (a, s') = read s 
-              in (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return x))) a s') 0
-     = let (a, s') = read 0
-        in (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return x))) a s'
-     = let (a, s') = (0, 0)
-        in (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return x))) a s'
-     = (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return x))) 0 0
-     = (write (0+1) >>= (\_ -> read >>= (\x -> return x)) 0
-     = ((\s -> ((), 1)) >>= (\_ -> read >>= (\x -> return x)) 0
-     = (\s -> let (a, s') = (\s -> ((), 1)) s 
-               in (\_ -> read >>= (\x -> return x)) a s') 0
-     = let (a,s') = (\s -> ((), 1)) 0
-        in (\_ -> read >>= (\x -> return x) a s')
-     = let (a,s') = ((), 1)
-        in (\_ -> read >>= (\x -> return x) a s')
-     = (\_ -> read >>= (\x -> return x) () 1
-     = (read >>= (\x -> return x)) 1
-
+     = (read >>= (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return (x+3)))) 0
      = (\s -> let (a, s') = read s
-               in (\x -> return x) a s') 1
+              in (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return (x+3)) a s') 0
+     = let (a, s') = read 0
+        in (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return (x+3)) a s'
+     = let (a, s') = (0, 0)
+        in (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return (x+3))) a s'
+     = (\x -> write (x+1) >>= (\_ -> read >>= (\x -> return (x+3))) 0 0
+     = write (0+1) >>= (\_ -> read >>= (\x -> return (x+3))) 0
+     = ((\_ -> ((), 1)) >>= (\_ -> read >>= (\x -> return x)) 0
+     = (\s -> let (a, s') = (\s -> ((), 1)) s
+               in (\_ -> read >>= (\x -> return (x+3))) a s') 0
+     = let (a,s') = (\s -> ((), 1)) 0
+       in (\_ -> read >>= (\x -> return (x+3)) a s')
+     = let (a,s') = ((), 1)
+        in (\_ -> read >>= (\x -> return (x+3)) a s')
+     = (\_ -> read >>= (\x -> return (x+3)) () 1
+     = (read >>= (\x -> return (x+3))) 1
+     = (\s -> let (a, s') = read s
+               in (\x -> return (x+3)) a s') 1
      = let (a, s') = read 1
-        in (\x -> return x) a s')
+        in (\x -> return (x+3)) a s'
      = let (a, s') = (1,1)
-        in (\x -> return x) a s')
-     = (\x -> return x) 1 1
-     = return 1 1
-     = (\s -> (1,s)) 1
-     = (1,1)
+        in (\x -> return (x+3)) a s'
+     = (\x -> return (x+3)) 1 1 
+     = return (1+3) 1
+     = (\s -> (4, s)) 1
+     = (4,1)
+
 
 -}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
